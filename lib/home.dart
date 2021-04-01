ab3b5eea89488f28875074d74f673691b3a8b559
Denise <villani.denise26@gmail.com>
2021-04-01 20:02:55 +0200

    Fab added

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +17,19 @@ class HomeState extends State<Home> {
       ),
       body: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text("Ciao"),
-            Text("Social Hackathon"),
-            Text("Mobile app"),
+            Text("Contatore"),
+            Text("$counter"),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState( () {
+          counter = counter + 1)
+        });
+        }
       ),
     );
   }
